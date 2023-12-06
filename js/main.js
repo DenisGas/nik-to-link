12759bb38cd4f91bfc9268094838acf5fa585848
Best_Komp <dehuc1811@gmail.com>
2023-12-06 18:25:18 +0200

    Added Example work in index.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const NIK_INPUT = document.getElementById("nik-textarea");
-const LINK_OUTPUT = document.getElementById("link-textarea");
+const LINK_OUTPUT = document.getElementById("link-text");
 const GET_LINK_BUTTON = document.getElementById("get-link-button");
 const COPY_BTN = document.getElementById("link-copy-btn")
 
@@ -10,25 +10,33 @@ if (TextAreaNikContent !== null) {
 };
 
 
-function showMessage(text) {
-  // Находим элемент контейнера сообщения
+NIK_INPUT.addEventListener("input", (e) => {
+  textValue = e.target.value;
+  callGetLinkFunc()
+});
+
+
+
+function showMessage(messageText) {
   const messageContainer = document.getElementById('message-container');
 
-  // Показываем сообщение, установив его стиль display в 'block'
-  messageContainer.style.display = 'block';
+  messageContainer.classList.toggle("smooth-appearance");
 
-  const messageText = text;
-
-  // Устанавливаем текст сообщения внутри контейнера
   messageContainer.textContent = messageText;
 
-  // Через некоторое время (например, 3 секунды) скрываем сообщение
   setTimeout(function () {
-    messageContainer.style.display = 'none';
-  }, 1000); // 3000 миллисекунд (3 секунды)
+    messageContainer.classList.toggle("smooth-appearance");
+  }, 1000);
 }
 
-
+const radios = document.getElementsByName("typesLink");
+radios.forEach(radio => {
+  radio.addEventListener("change", callGetLinkFunc);
+});
+const checkboxes = document.querySelectorAll(".config-box");
+checkboxes.forEach(box => {
+  box.addEventListener("change", callGetLinkFunc);
+});
 
 function getLinkType() {
   const radios = document.getElementsByName("typesLink");
@@ -36,22 +44,34 @@ function getLinkType() {
   return selected.value
 }
 
+
+
+function callGetLinkFunc() {
+  const text = NIK_INPUT.value;
+
+  const linkType = getLinkType();
+  const onlyLinks = getCheckBoxState("without-text");
+  const replaceSocialLinks = getCheckBoxState("replace-socials-links");
+  const link = getLink(text, linkType, onlyLinks, replaceSocialLinks);
+
+
+  LINK_OUTPUT.innerText = link;
+
+  if (link !== text) {
+    localStorage.setItem('TextAreaNik', text);
+  }
+
+  return true
+}
+
+
 GET_LINK_BUTTON.addEventListener("click", (event) => {
   if (NIK_INPUT.value !== "") {
     if (NIK_INPUT.classList.contains("error")) {
       NIK_INPUT.classList.remove("error")
     }
-    const text = NIK_INPUT.value;
 
-    localStorage.setItem('TextAreaNik', text);
-
-    const linkType = getLinkType()
-
-    const onlyLinks = getCheckBoxState("without-text")
-
-    const link = getLink(text, linkType, onlyLinks);
-
-    LINK_OUTPUT.value = link;
+    callGetLinkFunc()
 
     return true
 
@@ -59,24 +79,40 @@ GET_LINK_BUTTON.addEventListener("click", (event) => {
 
   NIK_INPUT.classList.add("error")
 
+  return false
+
 });
 
 
-COPY_BTN.addEventListener("click", () => {
-  const result = LINK_OUTPUT.value;
+COPY_BTN.addEventListener("click", (event) => {
+  copyToClipboard(LINK_OUTPUT.innerText)
+  event.target.disabled = true;
+  setTimeout(function () {
+    event.target.disabled = false;
+  }, 500);
 
-  navigator.clipboard.writeText(result).then(function () {
-    showMessage('Результат скопирован в буфер обмена');
+});
+
+
+
+function copyToClipboard(text) {
+  if (text === "" || text === null || text === undefined) {
+    showMessage('ERROR! No text');
+    return;
+  }
+
+  navigator.clipboard.writeText(text).then(function () {
+    showMessage('Result copied to clipboard');
   }).catch(function (error) {
-    showMessage('Не удалось скопировать результат в буфер обмена');
-    console.error('Не удалось скопировать результат в буфер обмена:', error);
+    showMessage('ERROR');
+    console.error('ERROR:', error);
   });
-});
-
+}
 
 function getLinkPath(linkType) {
   switch (linkType) {
     case 'instagram':
+    case 'inst':
       return "https://www.instagram.com/";
     case 'telegram':
       return "https://t.me/";
@@ -91,7 +127,7 @@ function getLinkPath(linkType) {
 
 
 
-function getLink(text, linkType = "instagram", onlyLinks = false,
+function getLink(text, linkType = "instagram", onlyLinks = false, replaceSocialLinks = false,
   delimiter = "") {
 
   const linkPath = getLinkPath(linkType);
@@ -100,16 +136,31 @@ function getLink(text, linkType = "instagram", onlyLinks = false,
     return linkPath;
   }
 
-  let link = ""
-  const modifiedString = text.replace(/@[^\s]+/g, match => {
-    const username = match.slice(1);
-    link += `${linkPath}${username}\n`;
-    return `${delimiter}${linkPath}${username}`;
-  });
-  if (!onlyLinks) {
-    return modifiedString;
+  const platformPattern = /(youtube|Telegram|inst|Instagram|TikTok):\s+@([\w.'\-_!#^~]+)/gi;
+  const usernamePattern = /(?<!\/)@([\w.'\-_!#^~]+)/g;
+
+  let modifiedString = text
+    .replace(platformPattern, (match, platform, username) => {
+      return `${platform}: ${getLinkPath(platform.toLowerCase())}${username}  `
+    })
+    .replace(usernamePattern, match => {
+      const username = match.slice(1);
+      return `${delimiter}${linkPath}${username} `;
+    });
+
+  if (onlyLinks) {
+    const onlyLinksPattern = /(https?:\/\/)(?:www\.)?([^\s]+)/g;
+    const matches = modifiedString.match(onlyLinksPattern);
+    modifiedString = matches ? matches.join("\n") : "";;
   }
-  return link;
+
+  if (replaceSocialLinks) {
+    const linkPattern = /(https:\/\/www\.instagram\.com\/|https:\/\/www\.youtube\.com\/@|https:\/\/t\.me\/|http:\/\/www\.tiktok\.com\/@)/g;
+    modifiedString = modifiedString.replace(linkPattern, linkPath);
+  }
+
+
+  return modifiedString;
 }
 
 function getCheckBoxState(id) {
